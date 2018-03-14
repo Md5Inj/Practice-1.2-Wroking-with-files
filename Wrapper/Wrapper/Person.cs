a5dec8f790792ef5cba809476a71f3cbd5d1bdd0
Maxim Romashko <md5inj@localhost>
2018-03-14 11:17:55 +0300

    Added class wrapper. Realized 50% of functions. Realized Person.cs

---
+++ Wrapper/Wrapper/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wrapper
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public uint Age { get; set; }
+        public char Gender { get; set; }
+    }
+}
---
+++ Wrapper/Wrapper/Wrapper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Wrapper
+{
+    class Wrapper
+    {
+        private LinkedList<Person> List;
+
+        Wrapper() // Default constructor
+        {
+            List.Clear();
+        }
+
+        void Insert(Person p) // Inserting element into end of list
+        {
+            List.AddLast(p);
+        }
+
+        void Remove()
+        {
+            List.Remove(List.First);
+        }
+
+        void Change(Person what, Person whom)
+        {
+            List.Find(what).Value = whom;
+        }
+
+        Person GetValue()
+        {
+            return List.First.Value;
+        }
+
+        LinkedListNode<Person> GetFirst()
+        {
+            return List.First;
+        }
+
+        LinkedListNode<Person> GetLast()
+        {
+            return List.Last;
+        }
+
+        LinkedListNode<Person> GetNext(LinkedListNode<Person> now)
+        {
+            return now.Next;
+        }
+
+        LinkedListNode<Person> GetPrev(LinkedListNode<Person> now)
+        {
+            return now.Previous;
+        }
+
+        void LoadFromFile(String fileName)
+        {
+
+        }
+    }
+}
